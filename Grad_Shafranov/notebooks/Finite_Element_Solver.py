e0541a15a3d291c08874824333a27c5655f96743
adynator <ady.kmec@gmail.com>
2026-05-21 16:43:28 +0100

    Update Finite_Element_Solver.py
    Added a Picard iteration and moved things around a bit to improve efficiency. Also fixed some bugs in the ordering of the basis nodes.

--- Grad_Shafranov/notebooks/Finite_Element_Solver.py
+++ Grad_Shafranov/notebooks/Finite_Element_Solver.py
@@ -29,17 +29,17 @@ class Finite_Element_Solver():
             return 0.5*l3*(3*l3-1)*(3*l3-2)
 
         def phi4(l1,l2,l3):
-            return 4.5*l1*l2*(3*l2-1)
-        def phi5(l1,l2,l3):
             return 4.5*l1*l2*(3*l1-1)
+        def phi5(l1,l2,l3):
+            return 4.5*l1*l2*(3*l2-1)
         def phi6(l1,l2,l3):
-            return 4.5*l2*l3*(3*l3-1)
-        def phi7(l1,l2,l3):
             return 4.5*l2*l3*(3*l2-1)
+        def phi7(l1,l2,l3):
+            return 4.5*l2*l3*(3*l3-1)
         def phi8(l1,l2,l3):
-            return 4.5*l1*l3*(3*l1-1)
-        def phi9(l1,l2,l3):
             return 4.5*l1*l3*(3*l3-1)
+        def phi9(l1,l2,l3):
+            return 4.5*l1*l3*(3*l1-1)
             
         def phi10(l1,l2,l3):
             return 27*l1*l2*l3
@@ -55,17 +55,17 @@ class Finite_Element_Solver():
             return np.array([0,0.5 * (27*l3*l3 - 18*l3 + 2)])
 
         def g4(l1,l2,l3):
-            return np.array([4.5 * (l2 * (3*l2 - 1) * (-1.0) + l1 * (6*l2 - 1) * 1.0),4.5 * (l2 * (3*l2 - 1) * (-1.0) + l1 * (6*l2 - 1) * 0.0)])
-        def g5(l1,l2,l3):
             return np.array([4.5 * (l2 * (6*l1 - 1) * (-1.0) + l1 * (3*l1 - 1) * 1.0),4.5 * (l2 * (6*l1 - 1) * (-1.0) + l1 * (3*l1 - 1) * 0.0)])
+        def g5(l1,l2,l3):
+            return np.array([4.5 * (l2 * (3*l2 - 1) * (-1.0) + l1 * (6*l2 - 1) * 1.0),4.5 * (l2 * (3*l2 - 1) * (-1.0) + l1 * (6*l2 - 1) * 0.0)])
         def g6(l1,l2,l3):
-            return np.array([ 4.5 * (l3 * (3*l3 - 1) * 1.0 + l2 * (6*l3 - 1) * 0.0),4.5 * (l3 * (3*l3 - 1) * 0.0 + l2 * (6*l3 - 1) * 1.0)])
-        def g7(l1,l2,l3):
             return np.array([ 4.5 * (l3 * (6*l2 - 1) * 1.0 + l2 * (3*l2 - 1) * 0.0),4.5 * (l3 * (6*l2 - 1) * 0.0 + l2 * (3*l2 - 1) * 1.0)])
+        def g7(l1,l2,l3):
+            return np.array([ 4.5 * (l3 * (3*l3 - 1) * 1.0 + l2 * (6*l3 - 1) * 0.0),4.5 * (l3 * (3*l3 - 1) * 0.0 + l2 * (6*l3 - 1) * 1.0)])
         def g8(l1,l2,l3):
-            return np.array([4.5 * (l1 * (3*l1 - 1) * 0.0 + l3 * (6*l1 - 1) * (-1.0)),4.5 * (l1 * (3*l1 - 1) * 1.0 + l3 * (6*l1 - 1) * (-1.0))])
-        def g9(l1,l2,l3):
             return np.array([ 4.5 * (l1 * (6*l3 - 1) * 0.0 + l3 * (3*l3 - 1) * (-1.0)),4.5 * (l1 * (6*l3 - 1) * 1.0 + l3 * (3*l3 - 1) * (-1.0))])
+        def g9(l1,l2,l3):
+            return np.array([4.5 * (l1 * (3*l1 - 1) * 0.0 + l3 * (6*l1 - 1) * (-1.0)),4.5 * (l1 * (3*l1 - 1) * 1.0 + l3 * (6*l1 - 1) * (-1.0))])
         
         def g10(l1,l2,l3):
             return np.array([27.0 * (-l2*l3 + l1*l3 ), 27.0 * (-l2*l3  + l1*l2 * 1.0)])
@@ -166,7 +166,7 @@ class Finite_Element_Solver():
                         return abs(a)*grad[j](l1,l2,l3)@J.T@J@grad[k](l1,l2,l3)/(xi[0]*l1 +xj[0]*l2+xk[0]*l3)
                     A[int(B[j]),int(B[k])] += self.IntegrateTriangle(f) 
         return A 
-    def LoadVector(self,Nx, Ny, x_i, x_f, y_i, y_f, Curr, T,V,TV):  
+    def LoadVector(self,Nx, Ny, x_i, x_f, y_i, y_f,T,V,TV,u):  
         #Construct load vector in FEM
         GDOF = 9*(Nx-1)*(Ny-1) +3*(Nx-1)+3*(Ny-1)+1
         b = np.zeros(GDOF)
@@ -179,7 +179,10 @@ class Finite_Element_Solver():
             B = TV[n,:]
             for j in range(10):
                 def f(l1,l2,l3):
-                    return abs(a)*phi[j](l1,l2,l3)*Curr(xi[0]*l1 +xj[0]*l2+ xk[0]*l3,xi[1]*l1 +xj[1]*l2+ xk[1]*l3 )
+                    U =0
+                    for k in range(10):
+                        U += u[int(TV[n,k])]*phi[k](l1,l2,l3)
+                    return abs(a)*phi[j](l1,l2,l3)*((xi[0]*l1 +xj[0]*l2+ xk[0]*l3)*(1+10*U**3)+3*U**2/(2*(xi[0]*l1 +xj[0]*l2+ xk[0]*l3)))
                 b[int(B[j])] += self.IntegrateTriangle(f) 
         return b
         
@@ -192,20 +195,30 @@ class Finite_Element_Solver():
             A[n,n] = 1
         return A, b, u
     def Solve(self,Nx, Ny, x_i, x_f, y_i, y_f):
-        T, V, TV = self.Square_Mesh(Nx, Ny, x_i, x_f, y_i, y_f)[:3]
+        T, V, TV, boundary_points  = self.Square_Mesh(Nx, Ny, x_i, x_f, y_i, y_f)
         A = self.StiffnessMatrix(Nx, Ny, x_i, x_f, y_i, y_f,T,V,TV)
-        def Curr(x,y):
-            return 10
-        b = self.LoadVector(Nx, Ny, x_i, x_f, y_i, y_f, Curr,T,V,TV)
-        GDOF = 9*(Nx-1)*(Ny-1) +3*(Nx-1)+3*(Ny-1)+1
-        u = np.zeros(GDOF)
-        boundary_points = self.Square_Mesh(Nx, Ny, x_i, x_f, y_i, y_f)[3]
         boundary_values = {}
         for key, n in boundary_points.items():
             boundary_values[key] = 0
-        A, b, u = self.BoundaryConditions(boundary_points, A, b, u, boundary_values)
-        A = A.tocsr()
-        u = spsolve(A, b)
+        GDOF = 9*(Nx-1)*(Ny-1) +3*(Nx-1)+3*(Ny-1)+1
+        u = np.zeros(GDOF)
+        converged = False
+        it = 0
+        while converged == False:    
+            b = self.LoadVector(Nx, Ny, x_i, x_f, y_i, y_f,T,V,TV,u)
+            A, b, u = self.BoundaryConditions(boundary_points, A, b, u, boundary_values)
+            u_old = u
+            A = A.tocsr()
+            u = spsolve(A, b)  
+            A = A.tolil()
+            diff = np.linalg.norm(u - u_old)
+            rel  = diff / (np.linalg.norm(u) + 1e-14)
+            print(rel)
+            it += 1
+            if rel < 1e-6:
+                converged = True
+                print("Number of Picard Iterations "+ str(it))
+            
         def U(x,y):
             U = 0
             k=0
@@ -221,13 +234,12 @@ class Finite_Element_Solver():
                         U += u[int(TV[n,j])]*phi[j](l1(x,y),l2(x,y),l3(x,y))
                 if k==1:
                     break
-            return U  
-            
+            return U    
         return u,U
 problem = Finite_Element_Solver()
 u, U = problem.Solve(10, 10, 1, 2, 1, 2)
-r = np.linspace(1, 2, 3*10-2)
-z = np.linspace(1, 2, 3*10-2)
+r = np.linspace(1, 2, 100)
+z = np.linspace(1, 2, 100)
 R, Z = np.meshgrid(r, z, indexing="ij")
 U_grid = np.vectorize(U)
 plt.pcolormesh(R, Z, U_grid(R,Z), shading='auto')
